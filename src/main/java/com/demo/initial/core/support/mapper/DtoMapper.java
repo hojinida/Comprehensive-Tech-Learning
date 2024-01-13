b689660d9cafbe0ebac52ac6bbed1464675b610d
hojinida <opqrs1103@gmail.com>
2024-01-14 02:01:15 +0900

    q

--- src/main/java/com/demo/initial/core/support/mapper/DtoMapper.java
+++ src/main/java/com/demo/initial/core/support/mapper/DtoMapper.java
@@ -7,6 +7,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring" )
 public interface DtoMapper {
+    
     MemberCreateResponseDto toMemberCreateResponseDto(Member member);
     SignInResponseDto toSignInResponseDto(String token);
 }
