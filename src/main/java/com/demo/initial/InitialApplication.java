2360e8ead935992a8569fd61d20e0a7ae166fb4f
hojinida <opqrs1103@gmail.com>
2024-01-13 21:07:28 +0900

    test: ci test

--- src/main/java/com/demo/initial/InitialApplication.java
+++ src/main/java/com/demo/initial/InitialApplication.java
@@ -13,7 +13,6 @@ public class InitialApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("KST"));
 		SpringApplication.run(InitialApplication.class, args);
-		
 	}
 
 }
